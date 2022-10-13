2148692175ef547311d1f274bf9388169415137a
Rushit <rushit@omtecweb.com>
2022-10-13 11:50:48 +0530

    Utils & constant files add

---
+++ olocker/lib/constants/app_images.dart
@@ -0,0 +1,5 @@
+class AppImages {
+  static const String imageRootPath = "assets/images";
+
+  static const String dealBgImage = '$imageRootPath/deal_bg_home.jpg';
+}
---
+++ olocker/lib/utils/extensions.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+extension CustomWidgets on Widget {
+  Widget commonAllSidePadding(double padding) {
+    return Padding(
+      padding: EdgeInsets.all(padding),
+      child: this,
+    );
+  }
+
+  Widget commonSymmetricPadding({double horizontal = 0, double vertical = 0}) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: horizontal, vertical: vertical),
+      child: this,
+    );
+  }
+}
