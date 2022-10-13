014df2f823c17c0bc86a31b8e1e30b71bf6dbcea
chirag0566 <107978521+chirag0566@users.noreply.github.com>
2022-10-13 12:40:41 +0530

    auth control

---
+++ olocker/lib/controllers/auth_screen_controller.dart
@@ -0,0 +1,17 @@
+import 'package:get/get.dart';
+
+class AuthScreenController extends GetxController {
+  final size = Get.size;
+
+  @override
+  void onInit() {
+    // TODO: implement onInit
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+}
