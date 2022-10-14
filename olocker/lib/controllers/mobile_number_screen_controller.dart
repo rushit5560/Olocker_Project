3d66b93fd7a3ab8ea2f24bf36bf2e34a82642561
chirag0566 <107978521+chirag0566@users.noreply.github.com>
2022-10-14 12:55:25 +0530

    mob screen controller

--- olocker/lib/controllers/mobile_number_screen_controller.dart
+++ olocker/lib/controllers/mobile_number_screen_controller.dart
@@ -3,11 +3,15 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:olocker/models/user_login_model.dart';
 import 'package:olocker/screens/index_screen/index_screen.dart';
 import 'package:olocker/screens/otp_screen/otp_screen.dart';
 import 'package:http/http.dart' as http;
 import 'package:olocker/screens/signup_screen/signup_screen.dart';
+import 'package:olocker/widgets/common_widgets.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../constants/api_url.dart';
+import '../constants/app_colors.dart';
 
 class MobileNumberScreenController extends GetxController {
   final size = Get.size;
@@ -19,81 +23,70 @@ class MobileNumberScreenController extends GetxController {
   final formKey = GlobalKey<FormState>();
 
   ApiHeader apiHeader = ApiHeader();
-  submitMobileNumber() async {
-    if (formKey.currentState!.validate()) {
-      try {
-        await checkMobileNumber();
-      } catch (e) {
-        log(e.toString());
-        rethrow;
-      }
-    }
-  }
 
-  Future<void> checkMobileNumber() async {
+  Future<void> userLoginFunction(BuildContext context) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+
     String mobNumber = numberController.text.toString();
-    isLoading(true);
-    String url =
-        "${ApiUrl.apiMainPath}${ApiUrl.checkUserByMobileApi}?mobileNo=$mobNumber";
-    log(" checkMobileNumber url: $url");
 
-    try {
-      http.Response response = await http.get(
-        Uri.parse(url),
-        headers: apiHeader.headers,
-      );
+    if (formKey.currentState!.validate()) {
+      isLoading(true);
+      String url = "${ApiUrl.checkUserByMobileApi}?mobileNo=$mobNumber";
+      log(" checkMobileNumber url: $url");
 
-      log("checkMobileNumber st code is : ${response.statusCode}");
-      log("checkMobileNumber res body : ${response.body}");
+      try {
+        http.Response response = await http.get(
+          Uri.parse(url),
+          headers: apiHeader.headers,
+        );
 
-      var resBody = json.decode(response.body);
-      var userMobBody = resBody["UserMobile"];
-      var isNumberExist = userMobBody["IsExist"];
+        log("checkMobileNumber st code is : ${response.statusCode}");
+        log("checkMobileNumber res body : ${response.body}");
 
-      log("checkMobileNumber user body : ${userMobBody}");
-      log("checkMobileNumber isexist : ${isNumberExist}");
+        var resBody = json.decode(response.body);
 
-      if (!isNumberExist) {
-        log("mobile number is new");
-        // Get.to(() => SignUpScreen());
-      } else {
-        log("mobile number is verfied");
-        Get.off(()=> OtpScreen());
+        UserLoginModel userLoginModel = UserLoginModel.fromJson(resBody);
+
+        var isSuccessStatus = userLoginModel.success;
+        var isCustomerExist = userLoginModel.isCustomer;
+
+        log("checkMobileNumber success  : ${isSuccessStatus}");
+        log("is customer exist : ${isCustomerExist}");
+
+        if (isCustomerExist == true) {
+          log("mobile number is verfied");
+          Get.off(() => OtpScreen());
+        } else {
+          final snackBar = SnackBar(
+            backgroundColor: AppColors.whiteColor,
+            elevation: 5,
+            behavior: SnackBarBehavior.floating,
+            margin: EdgeInsets.only(
+              left: 5,
+              right: 5,
+              bottom: 5,
+            ),
+            content: Text(
+              userLoginModel.errorInfo.extraInfo.isEmpty
+                  ? "User Not Registered"
+                  : userLoginModel.errorInfo.extraInfo,
+              style: TextStyle(
+                color: AppColors.blackColor,
+              ),
+            ),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+          log("mobile number is new");
+
+          // Get.to(() => SignUpScreen());
+        }
+      } catch (e) {
+        log("checkMobileNumber Error ::: $e");
+        rethrow;
+      } finally {
+        isLoading(false);
       }
-
-      // GetFcmTokeModel getFcmTokeModel = GetFcmTokeModel.fromJson(
-      //   json.decode(response.body),
-      // );
-      // isSuccessStatus = getFcmTokeModel.success.obs;
-      // log("getFcmTokeModel.success : ${getFcmTokeModel.success}");
-      // log("getFcmTokeModel.success : ${getFcmTokeModel.statusCode}");
-      // log("getFcmTokeModel.success : ${getFcmTokeModel.data.fcmToken}");
-
-      // if (isSuccessStatus.value) {
-      //   oppositeUserFcmToken = getFcmTokeModel.data.fcmToken;
-
-      //   /// Send Chat Notification
-      //   sendGeneralNotification(
-      //     fcmToken: oppositeUserFcmToken, // Getting From API
-      //     title: "${UserDetails.userName} booked appointment",
-      //     body:
-      //         "${UserDetails.userName} booked appointment for serviceName on $date at $slotTime",
-
-      //     type: 0,
-      //   );
-
-      //   message =
-      //       "${UserDetails.userName} booked appointment for serviceName on $date at $slotTime";
-
-      //   log("oppositeUserFcmToken : $oppositeUserFcmToken");
-      // } else {
-      //   log("getUserFcmTokenFunction Else Else");
-      // }
-    } catch (e) {
-      log("checkMobileNumber Error ::: $e");
-      rethrow;
-    } finally {
-      isLoading(false);
     }
   }
 
